181cddd1c418053f4a1894a69d2808f8c2a43acb
jeinieldjs <silvestrejeiniel@gmail.com>
2024-02-21 15:39:45 +0800

    Added production variables and seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,77 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+admin_user = User.create!(
+  first_name: "Admin",
+  last_name: "User",
+  email: "admin@example.com",
+  complete_address: "Admin Address",
+  province: "Samar",
+  city_municipality: "Santo Niño",
+  contact_number: "01234567890",
+  barangay: "Ilijan",
+  role: :admin,
+  status: :approved,
+  password: "adminpassword",
+  password_confirmation: "adminpassword"
+)
+
+User.create!(
+  first_name: "Jose",
+  last_name: "Reyes",
+  email: "jose@reyes.com",
+  complete_address: "123 Village 4, Brgy. Ilijan, Sto. Nino, Samar",
+  province: "Samar",
+  city_municipality: "Santo Niño",
+  contact_number: "09060000000",
+  barangay: "Ilijan",
+  role: :resident,
+  status: :approved,
+  password: "password",
+  password_confirmation: "password"
+)
+
+User.create!(
+  first_name: "Josefa",
+  last_name: "Reyes",
+  email: "josefa@reyes.com",
+  complete_address: "124 Village 4, Brgy. Ilijan, Sto. Nino, Samar",
+  province: "Samar",
+  city_municipality: "Santo Niño",
+  contact_number: "09060000001",
+  barangay: "Ilijan",
+  role: :resident,
+  status: :pending,
+  password: "password",
+  password_confirmation: "password"
+)
+
+User.create!(
+  first_name: "Joseph",
+  last_name: "Reyes",
+  email: "joseph@reyes.com",
+  complete_address: "124 Village 4, Brgy. Banlic, Calamba, Laguna",
+  province: "Laguna",
+  city_municipality: "City of Calamba",
+  contact_number: "09060000002",
+  barangay: "Banlic",
+  role: :resident,
+  status: :pending,
+  password: "password",
+  password_confirmation: "password"
+)
+
+User.create!(
+  first_name: "Admin",
+  last_name: "Banlic",
+  email: "admin@banlic.com",
+  complete_address: "Brgy Hall",
+  province: "Laguna",
+  city_municipality: "City of Calamba",
+  contact_number: "09060000003",
+  barangay: "Banlic",
+  role: :admin,
+  status: :approved,
+  password: "admin123",
+  password_confirmation: "admin123"
+)
